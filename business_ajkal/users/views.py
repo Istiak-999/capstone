9e06f0c44d7fa526f933948c8c85efc489c95f5a
Istiak Ahmed <istiak35-2967@diu.edu.bd>
2023-08-19 10:40:50 +0600

    final submission

---
+++ business_ajkal/users/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+import os
+# Create your views here.
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST or None, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST or None, request.FILES or None, instance=request.user.profilemodel)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('users-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profilemodel)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'users/profile.html', context)
